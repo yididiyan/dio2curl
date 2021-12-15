8c49fbd549bfd27e038dc6afbbe4edb2e4230788
Weizhong Yang <weizhong.yang@cerence.com>
2021-12-15 17:49:32 +0800

    Modernizes the project.

--- example/dio2curl_example.dart
+++ example/dio2curl_example.dart
@@ -2,27 +2,21 @@ import 'dart:convert';
 import 'package:dio2curl/dio2curl.dart';
 import 'package:dio/dio.dart';
 
-void main() async{
-  var dio = Dio();
+void main() async {
+  final dio = Dio();
 
   final data = {
     "title": "voluptate et itaque vero tempora molestiae",
-    "body": "eveniet quo quis laborum totam consequatur non dolor ut et est repudiandae est voluptatem vel debitis et magnam"
+    "body":
+        "eveniet quo quis laborum totam consequatur non dolor ut et est repudiandae est voluptatem vel debitis et magnam"
   };
-    
 
-  dio.post(
-    "https://jsonplaceholder.typicode.com/posts", 
-    data: json.encode(data),
-    options: Options(
-      responseType: ResponseType.json,
-      headers: {
-        'Authorization': 'Bearer asdkfjalskdfjSomebullshit',
-      },
-    
-  )).then((response) {
-    print(dio2curl(response.request));
-  });
-  
+  final response = await dio.post("https://jsonplaceholder.typicode.com/posts",
+      data: json.encode(data),
+      options: Options(
+        responseType: ResponseType.json,
+        headers: {'Authorization': 'Bearer asdkfjalskdfjSomebullshit'},
+      ));
 
+  print(response.requestOptions.toCurl());
 }
--- lib/dio2curl.dart
+++ lib/dio2curl.dart
@@ -1,28 +1,29 @@
 import 'package:dio/dio.dart';
 
-// A simple utility function to dump `curl` from "Dio" requests
+extension RequestOptionsToCurl on RequestOptions {
+  /// A simple utility function to dump `curl` from "Dio" requests.
+  String toCurl() => dio2curl(this);
+}
+
+/// A simple utility function to dump `curl` from "Dio" requests
 String dio2curl(RequestOptions requestOption) {
   var curl = '';
 
+  // Add PATH + REQUEST_METHOD
+  curl +=
+      'curl --request ${requestOption.method} \'${requestOption.baseUrl}${requestOption.path}\'';
 
-
-  // Add PATH + REQUEST_METHOD 
-  curl += 'curl --request ${requestOption.method} \'${requestOption.baseUrl}${requestOption.path}\'';
-  
-  // Include headers 
-  for(var key in requestOption.headers.keys) {
+  // Include headers
+  for (var key in requestOption.headers.keys) {
     curl += ' -H \'$key: ${requestOption.headers[key]}\'';
   }
 
-  // Include data if there is data 
-  if(requestOption.data != null) {
+  // Include data if there is data
+  if (requestOption.data != null) {
     curl += ' --data-binary \'${requestOption.data}\'';
   }
-  
-  curl += ' --insecure'; //bypass https verification
 
+  curl += ' --insecure'; //bypass https verification
 
   return curl;
 }
-
-
